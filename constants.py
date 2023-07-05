25393151e723d7d375970ecd9c6929af98daff43
Armaan Gupta <ohayouarmaan@gmail.com>
2023-07-05 17:27:23 +0530

    added the fields

---
+++ constants.py
@@ -0,0 +1,5 @@
+import os
+
+EXECUTABLE_PATH=os.path.abspath("./chromedriver.exe")
+print(EXECUTABLE_PATH)
+URL="http://app.cloudqa.io/home/AutomationPracticeForm"
---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import constants
+
+service = Service(executable_path=constants.EXECUTABLE_PATH)
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(constants.URL)
+
+def check_form():
+    form = driver.find_element(By.ID, "automationtestform")
+    pass
+
+def check_iframe_without_id():
+    pass
+
+def check_iframe_with_id():
+    pass
+
+if __name__ == "__main__":
+    check_form()
